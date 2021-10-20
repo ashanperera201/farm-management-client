5b49c8dd3a22cf757173487d3325e024e5e361f3
ashanperera201 <AshanPerera.201@gmail.com>
2021-10-20 15:46:27 +0530

    environment changes.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   // baseUrl: `https://farm-management-server-iw95j.ondigitalocean.app`,
-  baseUrl: `http://localhost:3001`,
+  // baseUrl: `http://localhost:3001`,
+  baseUrl: `https://farm-management-server.herokuapp.com`,
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   // baseUrl: `https://farm-management-server-iw95j.ondigitalocean.app`,
-  baseUrl: `http://localhost:3001`,
+  // baseUrl: `http://localhost:3001`,
+  baseUrl: `https://farm-management-server.herokuapp.com`,
   production: false
 };
 
